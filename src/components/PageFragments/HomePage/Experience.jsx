e96ccaa788998a68784d6c2c270fedadc69fbffb
hoangtien2k3 <hoangtien2k3qx1@gmail.com>
2026-01-05 02:56:40 +0700

    Refactor Experience section; adjust spacing and improve list formatting

--- src/components/PageFragments/HomePage/Experience.jsx
+++ src/components/PageFragments/HomePage/Experience.jsx
@@ -22,55 +22,34 @@ const Experience = () => (
         ]}
       />
       <h1 className="titleSeparate">Work Experience</h1>
-      <p style={{ marginBottom: '2em' }}>
+      <p style={{ marginBottom: '1em' }}>
         <Row>
           <Col xs={7}>
             <h3>VTI (VTI Group)</h3>
           </Col>
-          <Col xs={11} style={{ display: 'flex', justifyContent: 'center' }}>
-            <h3> </h3>
-          </Col>
-          <Col xs={6} style={{ display: 'flex', justifyContent: 'end' }}>
-            <h3> </h3>
-          </Col>
         </Row>
-        <li>
-          Develop the Mobi system (a ride-hailing system for 3 markets: Japan, Malaysia, and Vietnam)
-        </li>
-        <li>
-          Developing a facial recognition payment system for buses in Japan.
-        </li>
-        {' '}
+        <li style={{ marginLeft: '1rem' }}>Develop the Mobi system (a ride-hailing system for 3 markets: Japan, Malaysia, and Vietnam)</li>
+        <li style={{ marginLeft: '1rem' }}>Developing a facial recognition payment system for buses in Japan.</li>
       </p>
 
-      <p style={{ marginBottom: '2em' }}>
+      <p style={{ marginBottom: '1em' }}>
         <Row>
           <Col xs={7}>
             <h3>Viettel Telecom</h3>
           </Col>
         </Row>
-        <li>Developing a Java backend microservice system (SME-HUB)</li>
-        <li>
-          Java (SpringBoot Framework), MVC pattern, BPMN
-          flow, SOAP API Ehcache, Jenkin (CI/CD), K8S, Docker
-        </li>
-        {' '}
+        <li style={{ marginLeft: '1rem' }}>Developing a Java backend microservice system (SME-HUB)</li>
+        <li style={{ marginLeft: '1rem' }}>Java (SpringBoot Framework), MVC pattern, BPMN flow, SOAP API Ehcache, Jenkin (CI/CD), K8S, Docker</li>
       </p>
 
-      <p style={{ marginBottom: '2em' }}>
+      <p style={{ marginBottom: '1em' }}>
         <Row>
           <Col xs={7}>
             <h3>Cen Land (Cen Group)</h3>
           </Col>
         </Row>
-        <li>
-          Actively contributed to developing an Android application (Fastee)
-        </li>
-        <li>
-          Focused on golf course booking alongside building foundational backend
-          components to support the app’s functionality.
-        </li>
-        {' '}
+        <li style={{ marginLeft: '1rem' }}>Actively contributed to developing an Android application (Fastee)</li>
+        <li style={{ marginLeft: '1rem' }}>Focused on golf course booking alongside building foundational backend components to support the app’s functionality.</li>
       </p>
     </div>
   </div>
